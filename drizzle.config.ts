8f2122ddf85eb636242b81a5f65e7fafce15d810
Armaan Chahal <armaan.c2009@gmail.com>
2025-09-25 22:59:39 -0500

    fix lint errors

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/lib/drizzle/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
 } satisfies Config
--- src/lib/drizzle/index.ts
+++ src/lib/drizzle/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const connectionString = process.env.DATABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL!
+const connectionString = process.env.POSTGRES_URL!
 
 // Create the connection
 const client = postgres(connectionString, { prepare: false })
