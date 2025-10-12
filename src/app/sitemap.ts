7421fdf4dd56dd27bb79876be745800a13bd4a92
Armaan Chahal <armaan.c2009@gmail.com>
2025-10-11 23:49:28 -0500

    Add pricing page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -41,5 +41,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: "monthly",
             priority: 0.6,
         },
+
+        {
+            url: `${baseUrl}/pricing`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.5,
+        }
     ];
 }
