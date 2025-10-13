5343c5f277a1b86fb2455119b8cc558e3cfe79a9
Armaan Chahal <armaan.c2009@gmail.com>
2025-10-13 09:30:38 -0500

    Remove ssr false for ClerkProviderLazy to fix issues with navigation directly to protected routes

--- src/components/clerk/ClerkProviderLazy.tsx
+++ src/components/clerk/ClerkProviderLazy.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic'
 
 const ClerkClientProvider = dynamic(() => import("./ClerkClientProvider"), {
-    ssr: false,
+    // had to remove ssr: false as it was causing issues if users navigated straight to a protected route
 })
 
 export default ClerkClientProvider
